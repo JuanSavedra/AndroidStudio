c123b79904883d80b5e6b4164c178747585fb92b
JuanSavedra <juansavedra827@gmail.com>
2025-08-12 21:45:22 -0300

    first commit

---
+++ app/src/main/java/br/com/savedra/androidstudio/MainActivity.kt
@@ -0,0 +1,40 @@
+package br.com.savedra.androidstudio
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.savedra.androidstudio.ui.theme.AndroidStudioTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidStudioTheme {
+                Greeting()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidStudioTheme {
+        Greeting()
+    }
+}
