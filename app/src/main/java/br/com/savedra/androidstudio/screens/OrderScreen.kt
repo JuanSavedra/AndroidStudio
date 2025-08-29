9819e8403a1016fd6f49e10cb96566fec2b6cc15
JuanSavedra <juansavedra827@gmail.com>
2025-08-29 18:14:10 -0300

    creating screens

---
+++ app/src/main/java/br/com/savedra/androidstudio/screens/OrderScreen.kt
@@ -0,0 +1,84 @@
+package br.com.savedra.androidstudio.screens
+
+import android.R
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun OrderScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+            .background(Color(0xFFAFA9A9))
+            .padding(32.dp)
+    ) {
+        Text(
+            text = "PEDIDOS",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+        Card (
+            modifier = Modifier.fillMaxWidth()
+                .padding(top = 32.dp)
+        ) {
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth()
+                    .padding(12.dp),
+                colors = ButtonDefaults.buttonColors(Color.White)
+            ) {
+                Text(
+                    text = "Tênis",
+                    fontSize = 20.sp,
+                    color = Color.Black
+                )
+            }
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth()
+                    .padding(12.dp),
+                colors = ButtonDefaults.buttonColors(Color.White)
+            ) {
+                Text(
+                    text = "Roupas",
+                    fontSize = 20.sp,
+                    color = Color.Black
+                )
+            }
+        }
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(Color.White),
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            Text(
+                text = "Voltar",
+                fontSize = 20.sp,
+                color = Color.Blue
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewOrderScreen() {
+    OrderScreen()
+}
